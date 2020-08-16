5a2ebaf4b119cc873a44ecc8fee9aa3e63c7c1d0
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2020-08-16 12:50:06 +0200

    Testing render method - works well

--- game.py
+++ game.py
@@ -11,21 +11,27 @@ wn.tracer(0)
 # ----------- All Classes Space ----------------
 
 class Sprite(turtle.Turtle):
-    def __init__(self, x, y, color):
+    def __init__(self, x, y, color, shape):
         turtle.Turtle.__init__(self)
         self.x = x
         self.y = y
         self.color = color
+        self.shape = shape
         self.pen = turtle.Turtle()
 
     def render(self):
         self.pen.goto(self.x, self.y)
+        self.pen.color(self.color)
+        self.pen.shape(self.shape)
         self.pen.stamp()
 
 
 # ----------- End of All Classes Space ---------
 
+sprite = Sprite(100, 100, "red", "square")
+
 
 while True:
     wn.update()
+    sprite.render()
 
