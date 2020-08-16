79ec0117929cf20493911bdeb2419a9fab6be200
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2020-08-16 12:41:58 +0200

    Adding Sprite class and adding proper speed of window refresh.

--- game.py
+++ game.py
@@ -5,6 +5,18 @@ import math
 wn = turtle.Screen()
 wn.bgcolor("black")
 wn.title("Advanced Pong Game by Mateusz Hyla | @HylaTech")
+wn.tracer(0)
+
+
+# ----------- All Classes Space ----------------
+
+class Sprite(turtle.Turtle):
+    def __init__(self):
+        turtle.Turtle.__init__(self)
+
+
+# ----------- End of All Classes Space ---------
+
 
 while True:
     wn.update()
