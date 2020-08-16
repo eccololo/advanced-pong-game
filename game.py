1f0d4c22b2b2f3f62cac940b34ec2647534f28ec
Mateusz Hyla <mateusz.hyla.it@gmail.com>
2020-08-16 12:56:34 +0200

    Solving size issues with Sprite class.

--- game.py
+++ game.py
@@ -11,35 +11,41 @@ wn.tracer(0)
 # ----------- All Classes Space ----------------
 
 class Sprite(turtle.Turtle):
-    def __init__(self, x, y, color, shape):
+    def __init__(self, x, y, color, shape, size):
         turtle.Turtle.__init__(self)
         self.x = x
         self.y = y
         self.color = color
         self.shape = shape
+        self.size = size
         self.pen = turtle.Turtle()
 
     def render(self):
         self.pen.goto(self.x, self.y)
         self.pen.color(self.color)
         self.pen.shape(self.shape)
+        self.pen.pensize(self.size)
         self.pen.stamp()
-        
+
     def update(self):
         pass
 
+    def move_down(self):
+        self.y -= 10
+
+    def move_up(self):
+        self.y += 10
 
 
 # ----------- End of All Classes Space ---------
 
-sprite = Sprite(100, 100, "red", "square")
+sprite = Sprite(100, 100, "red", "square", 20)
 
 # ----------- Keyboard Binding Space -----------
 wn.listen()
-wn.onkeypress("move_down", "Down")
-wn.onkeypress("move_up", "Up")
+wn.onkeypress(sprite.move_down, "Down")
+wn.onkeypress(sprite.move_up, "Up")
 
 while True:
     wn.update()
     sprite.render()
-
